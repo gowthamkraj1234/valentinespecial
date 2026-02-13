b672b3fcb54b36b6c9fcb7722c3d7c9c88793115
Gowtham k <gowthamkraj1234@gmail.com>
2026-02-13 11:19:16 +0530

    Delete Python directory

--- Python/palindrome.py
+++
@@ -1,7 +0,0 @@
-#palindrome
-string = input("Enter a string: ")
-reversed_string = string[::-1]
-if string == reversed_string:
-    print(f"{string} is a palindrome")
-else:
-    print(f"{string} is not a palindrome")
